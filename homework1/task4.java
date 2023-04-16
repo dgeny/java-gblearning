2f2c8bc7854e7041d63d1ea27ad23f24d98939ed
Eugene N <kan1fol@ya.ru>
2023-04-16 18:13:32 +0200

    Homework 1: cosmetics

--- homework1/task4.java
+++ homework1/task4.java
@@ -4,7 +4,6 @@ package homework1;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.regex.*;
 
 public class task4 {
--- homework1/task5/program.java
+++ homework1/task5/program.java
@@ -2,7 +2,6 @@ package homework1.task5;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.ArrayList;
@@ -32,13 +31,11 @@ public class program {
                         System.out.println(String.join("\n",variants));
                     }
                 }
-                else if (str.charAt(0) == 'q')
+                else if (str.charAt(0) != 'q')
                 {
-                    
-                }
-                else{
                     System.out.println("Введено некорректное выражение");
                 }
+
             } while (str.charAt(0) != 'q');
             br.close();
         }
